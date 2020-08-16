d74d9fc76983de846c0d04b318fbe9d2f3c5048c
william <william.del.rosario@iceshop.nl>
2020-08-16 09:54:30 +0200

    Setting Up Request Logging

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const flash = require('connect-flash');
 const multer = require('multer');
 const helmet = require('helmet');
 const compression = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -53,8 +55,11 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a'});
+
 app.use(helmet());
 app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
